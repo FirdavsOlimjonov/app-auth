cbd3e1da2c1b48f44c6ef3b6cb0f3363eb2f1b6f
shafaq0206 <shafaqruzimuratov533@gmail.com>
2022-09-28 14:18:16 +0500

    Shafaq

---
+++ src/main/java/uz/pdp/payload/EmployeeDTO.java
@@ -0,0 +1,17 @@
+package uz.pdp.payload;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String middleName;
+
+    private String phoneNumber;
+
+    private String role_String;
+}
