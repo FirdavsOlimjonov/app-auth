9c9d65867cc06eb5aef609043c29a67c4c912fc9
shafaq0206 <shafaqruzimuratov533@gmail.com>
2022-09-28 15:46:05 +0500

    ShafaqAfterLogic123

--- src/main/java/uz/pdp/entity/Employee.java
+++ src/main/java/uz/pdp/entity/Employee.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
