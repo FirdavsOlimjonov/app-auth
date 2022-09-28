fb6419a7a099cc3758dcc068e45bcbab46921db3
Abdullayev <sherzodabdullayev5109@gmail.com>
2022-09-28 14:08:13 +0500

    Ikrom akaga

--- src/main/java/uz/pdp/controller/AuthControllerImpl.java
+++ src/main/java/uz/pdp/controller/AuthControllerImpl.java
@@ -1,11 +1,11 @@
-package uz.pdp.controller.implementation;
+package uz.pdp.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import uz.pdp.controller.cotract.AuthController;
+import uz.pdp.controller.AuthController;
 import uz.pdp.payload.ApiResult;
 import uz.pdp.payload.SignDTO;
 import uz.pdp.payload.TokenDTO;
--- src/main/java/uz/pdp/controller/RoleController.java
+++ src/main/java/uz/pdp/controller/RoleController.java
@@ -1,4 +1,4 @@
-package uz.pdp.controller.cotract;
+package uz.pdp.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
