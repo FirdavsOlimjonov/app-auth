c88c464c58ffc4c5e0d81dd049cf9a45647d0b64
SirojdidinSaidov <sirojdidinecma@gmail.com>
2022-09-29 11:03:09 +0500

    FIrst

--- src/main/java/uz/pdp/entity/Currier.java
+++ src/main/java/uz/pdp/entity/Currier.java
@@ -8,7 +8,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.Collection;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -17,7 +17,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @DynamicUpdate
 @DynamicInsert
-public class Employee {
+public class Currier {
 
     @Id
     @GeneratedValue(generator = "UUID")
@@ -25,16 +25,20 @@ public class Employee {
             strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
 
+    @JoinColumn(unique = true)
+    @OneToOne(optional = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
     @Column(nullable = false)
     private String firstName;
 
     @Column(nullable = false)
     private String lastName;
 
-    @JoinColumn(unique = true)
-    @OneToOne(optional = false)
-    private User user;
+    private String carNumber;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Role role;
+    private String driverLicense;
 }
