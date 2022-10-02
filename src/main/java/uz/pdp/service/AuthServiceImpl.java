49fc5f2a01a424fc3ed392ccd44b29316f2b048b
Abdullayev <sherzodabdullayev5109@gmail.com>
2022-10-02 17:50:45 +0500

    Oct 2 17:50

--- src/main/java/uz/pdp/service/AuthServiceImpl.java
+++ src/main/java/uz/pdp/service/AuthServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -215,4 +216,9 @@ public class AuthServiceImpl implements AuthService {
 //        javaMailSender.send(mailMessage);
     }
 
+    @Override
+    public User getUserByToken(String token) {
+
+        return null;
+    }
 }
