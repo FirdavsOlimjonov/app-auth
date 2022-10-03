30ee040caf2c3b7049aa4816a2633e92283ea2a5
Abdullayev <sherzodabdullayev5109@gmail.com>
2022-10-03 19:23:00 +0500

    Client done

--- src/main/java/uz/pdp/controller/ClientController.java
+++ src/main/java/uz/pdp/controller/ClientController.java
@@ -6,28 +6,33 @@ import uz.pdp.entity.Client;
 import uz.pdp.payload.ApiResult;
 import uz.pdp.payload.add_DTO.AddClientDTO;
 import uz.pdp.payload.response_DTO.ClientDTO;
+import uz.pdp.util.RestConstants;
 
 import java.util.List;
 import java.util.UUID;
 
 
-@RequestMapping("/api/client")
+@RequestMapping(ClientController.CLIENT_CONTROLLER_BASE_PATH)
 public interface ClientController {
 
-    @GetMapping("/get-all")
+    String CLIENT_CONTROLLER_BASE_PATH = RestConstants.SERVICE_BASE_PATH + "client";
+    String GET_ALL_PATH = "/get-all";
+    String GET_BY_ID_PATH = "/{id}";
+    String DELETE_BY_ID_PATH = "/{id}";
+
+    @GetMapping(GET_ALL_PATH)
     ApiResult<List<ClientDTO>> getAll();
 
-    @GetMapping("/{id}")
+    @GetMapping(GET_BY_ID_PATH)
     ApiResult<ClientDTO> get(@PathVariable UUID id);
 
     @PostMapping
     ApiResult<Boolean> add(@RequestBody AddClientDTO addClientDTO);
 
-
     @PutMapping
     ApiResult<Boolean> edit(@RequestBody ClientDTO clientDTO);
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(DELETE_BY_ID_PATH)
     ApiResult<Boolean> delete(@PathVariable UUID id);
 
 }
