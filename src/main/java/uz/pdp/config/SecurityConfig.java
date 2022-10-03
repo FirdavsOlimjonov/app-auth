02fae1c955a5b04b2bd1725f74f802d3061d9620
Abdullayev <sherzodabdullayev5109@gmail.com>
2022-10-03 15:04:56 +0500

    ?

---
+++ src/main/java/uz/pdp/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package uz.pdp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uz.pdp.exceptions.MyEntryPointHandler;
+import uz.pdp.security.JWTFilter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final MyEntryPointHandler myEntryPointHandler;
+
+    private final JWTFilter jwtFilter;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests(
+                        auth ->
+                                auth
+                                        .antMatchers("/api/auth/**")
+                                        .permitAll()
+                                        .antMatchers(HttpMethod.OPTIONS)
+                                        .permitAll()
+                                        .antMatchers("/",
+                                                "/favicon.ico",
+                                                "//*.png",
+                                                "//*.gif",
+                                                "//*.svg",
+                                                "//*.jpg",
+                                                "//*.html",
+                                                "//*.css",
+                                                "//*.js",
+                                                "/swagger-ui.html",
+                                                "/swagger-resources/",
+                                                "/v2/",
+                                                "/csrf",
+                                                "/webjars/",
+                                                "/v2/api-docs",
+                                                "/configuration/ui")
+                                        .permitAll()
+                                        .antMatchers("/api/**")
+                                        .authenticated())
+                .exceptionHandling()
+                .authenticationEntryPoint(myEntryPointHandler)
+                .and()
+//                .rememberMe()
+//                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
