dfff087ef1e2a4bc98225c060755ae1a05d0926e
Firdavs <firdavs_olimjonov@student.itpu.uz>
2026-06-24 11:38:01 +0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    io::stdin().read_line(&mut command).unwrap();
     print!("$ ");
     io::stdout().flush().unwrap();
 }
