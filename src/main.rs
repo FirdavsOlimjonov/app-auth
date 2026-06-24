bab349b9ada1d1a7c6bf930867f80c4abf8f20c9
Firdavs <firdavs_olimjonov@student.itpu.uz>
2026-06-24 11:49:47 +0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim());
+        // Wait for user input
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        println!("{}: command not found", command.trim());
+    }
+
 }
